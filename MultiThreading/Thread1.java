bd9d1ecb69cfe6d1e5bb9fd14750740b25f2ee14
Anbu A <anbarasuannadurai7@gmail.com>
2025-12-19 17:59:46 +0530

    created Thread

---
+++ MultiThreading/Thread1.java
@@ -0,0 +1,15 @@
+package MultiThreading;
+
+public class Thread1 extends Thread {
+
+    public Thread1(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("inside : " + Thread.currentThread() +" : "+ i);
+        }
+    }
+}
---
+++ MultiThreading/ThreadTester.java
@@ -0,0 +1,18 @@
+package MultiThreading;
+
+public class ThreadTester {
+
+    public static void main(String[] args) {
+        System.out.println("Main is Starting");
+
+        Thread thread1 = new Thread1("Thread1");
+//        thread1.setDaemon(true);
+        thread1.start();
+
+        Thread thread2 = new Thread(new Thread2(), "Thread2");
+        thread2.start();
+
+        System.out.println("Main is Exiting");
+    }
+
+}

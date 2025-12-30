a037b886dc362f87fea31f95413fd2afebe18d9a
Anbu A <anbarasuannadurai7@gmail.com>
2025-12-30 14:51:01 +0530

    added UC2

--- OOPs_Banner_App/OopsBannerApp.java
+++ OOPs_Banner_App/OopsBannerApp.java
@@ -4,10 +4,49 @@ public class OopsBannerApp {
 
     public static void main(String[] args) {
         uc1();
+        uc2();
     }
 
     public static void uc1() {
         String str = "OOPS";
         System.out.println(str);
+    }
+
+    public static void uc2() {
+        String[] O = {
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+        String[] P = {
+                "*******  ",
+                "*      * ",
+                "*      * ",
+                "*******  ",
+                "*        ",
+                "*        ",
+                "*        "
+        };
+        String[] S = {
+                " ******* ",
+                " *       ",
+                " *       ",
+                " ******* ",
+                "       * ",
+                "       * ",
+                " ******* "
+        };
+        String[][] oops = {O, O, P, S};
+        for (int row = 0; row < O.length; row++) {
+            StringBuilder line = new StringBuilder();
+            for (String[] letter : oops) {
+                line.append(letter[row]).append(" ");
+            }
+            System.out.println(line.toString());
+        }
     }
 }

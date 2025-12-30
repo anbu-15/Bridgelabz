7ebb1d08164be4d98d44d914a91d570188ad62db
Anbu A <anbarasuannadurai7@gmail.com>
2025-12-30 14:49:38 +0530

    added UC1

---
+++ OOPs_Banner_App/OopsBannerApp.java
@@ -0,0 +1,13 @@
+package OOPs_Banner_App;
+
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+        uc1();
+    }
+
+    public static void uc1() {
+        String str = "OOPS";
+        System.out.println(str);
+    }
+}
